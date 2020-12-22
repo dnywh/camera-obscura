9882a29b840731c91dc9a7bf58a4f52e46bd0e45
Danny White <hi@dannywhite.site>
2020-12-22 15:40:43 +1000

    working data and sorting

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,18 @@ module.exports = config => {
     config.addPassthroughCopy("./src/css");
     config.addPassthroughCopy("./src/scripts");
 
+    config.addCollection("photography", function (collection) {
+        // Collect any items in the photography directory
+        return collection.getFilteredByGlob("./src/photography/*.*").sort((a, b) => {
+            // Sort these items
+            if (a.data.title > b.data.title) return -1;
+            else if (a.data.title < b.data.title) return 1;
+            else return 0;
+            // And reverse to start with earlier years first
+        }).reverse();
+    });
+
+
     return {
         // Lets me use Nunjunks for everything
         markdownTemplateEngine: 'njk',
